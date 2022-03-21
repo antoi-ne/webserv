5871043098126365872b2b167405fa47108059e1
Victor Neirinckx <vneirinc@fu-r9-p4.s19.be>
2022-03-21 16:40:17 +0100

    remove 405 test

--- tester/test_wrong_method.py
+++ tester/test_wrong_method.py
@@ -2,7 +2,4 @@ from utils_test import req_check
 
 def test_invalid():
 	req_check("spj / HTTP/1.1\r\nhost: x\r\n\r\n", 400)
-
-def test_unrecognize():
-	req_check("spj / HTTP/1.1\r\nhost: x\r\n\r\n", 400)
-	req_check("SPJ / HTTP/1.1\r\nhost: x\r\n\r\n", 405)
+	req_check("GeT / HTTP/1.1\r\nhost: x\r\n\r\n", 400)
